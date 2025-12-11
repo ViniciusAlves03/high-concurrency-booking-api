3ae3f79a098046f53bba47112a74bb653e82475e
Vinícius Alves <vinicius.alves0503@gmail.com>
2025-12-11 17:22:15 -0300

    initial commit

---
+++ src/main/java/org/hcb/highconcurrencybookingapi/HighConcurrencyBookingApiApplication.java
@@ -0,0 +1,13 @@
+package org.hcb.highconcurrencybookingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HighConcurrencyBookingApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HighConcurrencyBookingApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/hcb/highconcurrencybookingapi/TestHighConcurrencyBookingApiApplication.java
@@ -0,0 +1,11 @@
+package org.hcb.highconcurrencybookingapi;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestHighConcurrencyBookingApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.from(HighConcurrencyBookingApiApplication::main).with(TestcontainersConfiguration.class).run(args);
+    }
+
+}
